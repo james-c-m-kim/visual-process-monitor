0fb5ee0c88c1597d28e6c40deadce7d0c10e86c7
kimjc <kimjc@code4lifesoftware.com>
2018-02-20 21:32:32 -0500

    initial...

--- VisualProcessMonitor/MainWindow.xaml.cs
+++ VisualProcessMonitor/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Windows.Threading;
 
 namespace VisualProcessMonitor
 {
@@ -20,9 +15,52 @@ namespace VisualProcessMonitor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IntPtr hwnd;
+        private Process proc;
+        private IDisposable subscription;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Capture_Click(object sender, RoutedEventArgs e)
+        {
+            var bmp = ScreenCaptureUtil.PrintWindow(hwnd);
+
+            var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var saveFile = Path.Combine(folder, "screencap.png");
+            if (File.Exists(saveFile))
+            {
+                File.Delete(saveFile);
+            }
+
+            bmp.Save(saveFile, ImageFormat.Png);
+        }
+
+        private void Process_Start_Click(object sender, RoutedEventArgs e)
+        {
+            proc = Process.Start("cmd.exe");
+
+            // async task to pick up the mainwindowhandle because:
+            // the mainwindowhandle gets updated after the console window is created...
+            // yarp.
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(100);
+
+                if (proc.MainWindowHandle != IntPtr.Zero)
+                {
+                    hwnd = proc.MainWindowHandle;
+                    btnCapture.Dispatcher.BeginInvoke(DispatcherPriority.Background,
+                        new Action(() =>
+                        {
+                            btnCapture.IsEnabled = true;
+                        }));
+                }
+            });
+        }
+
+
     }
 }
---
+++ VisualProcessMonitor/ScreenCaptureUtil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace VisualProcessMonitor
+{
+    public class ScreenCaptureUtil
+    {
+        private enum ShowWindowEnum
+        {
+            Hide = 0,
+            ShowNormal = 1, ShowMinimized = 2, ShowMaximized = 3,
+            Maximize = 3, ShowNormalNoActivate = 4, Show = 5,
+            Minimize = 6, ShowMinNoActivate = 7, ShowNoActivate = 8,
+            Restore = 9, ShowDefault = 10, ForceMinimized = 11
+        };
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [DllImport("user32.dll")]
+        public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, ShowWindowEnum flags);
+
+        public static Bitmap PrintWindow(IntPtr hwnd)
+        {
+            ShowWindow(hwnd, ShowWindowEnum.Restore);
+
+            RECT rc;
+            GetWindowRect(hwnd, out rc);
+
+            Bitmap bmp = new Bitmap(rc.Width, rc.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Graphics gfxBmp = Graphics.FromImage(bmp);
+            IntPtr hdcBitmap = gfxBmp.GetHdc();
+
+            PrintWindow(hwnd, hdcBitmap, 0);
+
+            gfxBmp.ReleaseHdc(hdcBitmap);
+            gfxBmp.Dispose();
+
+            return bmp;
+        }
+
+    }
+}
